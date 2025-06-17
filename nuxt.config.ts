4843c2406107b3caff15f161655cff2ee9b787fe
Dihan <rafeeuzzamandihan@gmail.com>
2025-06-17 15:55:55 +0600

    initial

---
+++ nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: [
+    '@pinia/nuxt',
+    '@nuxtjs/tailwindcss',
+  ],
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true }
+})
